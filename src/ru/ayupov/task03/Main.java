2625c45e9968990f054f6731469e0f4287b0a445
Firzar Ayupov <a.firzar.r@gmail.com>
2020-06-10 00:06:22 +0300

    Написал 3 программу по дз 4 занятия

---
+++ src/ru/ayupov/task03/Main.java
@@ -0,0 +1,30 @@
+package ru.ayupov.task03;
+
+import java.util.Scanner;
+
+/**
+ * Домашнее задание №4
+ * Программа №3
+ *
+ * @author Фирзар Аюпов
+ * <p>
+ * Написать программу для вывода на экран таблицы умножения.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Программа вывода на экран таблицы умножения");
+        System.out.println("Введите размер таблицы по вертикали (положительное целое число):");
+        Scanner scn = new Scanner(System.in);
+        int tableHeight = scn.nextInt();
+        System.out.println("Введите размер таблицы по горизонтали (положительное целое число):");
+        int tableWidth = scn.nextInt();
+
+        for (int i = 1; i < tableHeight + 1; i++) {
+            for (int k = 1; k < tableWidth + 1; k++) {
+                System.out.printf("%3d", (k * i));
+            }
+            System.out.println("");
+        }
+    }
+}
