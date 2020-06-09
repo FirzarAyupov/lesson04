c045daa71e6cbfff57d413e2e4d0e88214a11e21
Firzar Ayupov <a.firzar.r@gmail.com>
2020-06-10 00:55:38 +0300

    Добавлены отступы для удобного чтения кода

--- src/ru/ayupov/task02/Main.java
+++ src/ru/ayupov/task02/Main.java
@@ -15,9 +15,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Программа описания введенного числа");
+
         System.out.println("Введите число:");
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
+
         System.out.print("Введенное число " + num + ": ");
 
         if (num == 0) {
