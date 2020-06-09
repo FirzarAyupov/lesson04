55b910986d9c97887d1fd7d62f730a4ee60a7984
Firzar Ayupov <a.firzar.r@gmail.com>
2020-06-10 01:02:05 +0300

    Добавлена проверка ввденных данных, добавлены отступы

--- src/ru/ayupov/task03/Main.java
+++ src/ru/ayupov/task03/Main.java
@@ -14,17 +14,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Программа вывода на экран таблицы умножения");
+
         System.out.println("Введите размер таблицы по вертикали (положительное целое число):");
         Scanner scn = new Scanner(System.in);
         int tableHeight = scn.nextInt();
+
         System.out.println("Введите размер таблицы по горизонтали (положительное целое число):");
         int tableWidth = scn.nextInt();
 
-        for (int i = 1; i < tableHeight + 1; i++) {
-            for (int k = 1; k < tableWidth + 1; k++) {
-                System.out.printf("%3d", (k * i));
+        if (tableHeight > 0 && tableWidth > 0) {
+            for (int i = 1; i < tableHeight + 1; i++) {
+                for (int k = 1; k < tableWidth + 1; k++) {
+                    System.out.printf("%3d", (k * i));
+                }
+                System.out.println("");
             }
-            System.out.println("");
+        } else {
+            System.out.println("Введенные размеры таблицы не соответствуют требованиям");
         }
+
+
     }
 }
