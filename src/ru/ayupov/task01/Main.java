082323a0e3795b2f69af8c9f57ef80be353f81e8
Firzar Ayupov <a.firzar.r@gmail.com>
2020-06-09 12:42:16 +0300

    Отредактировал описание задачи и дополнил выводимую информацию

--- src/ru/ayupov/task01/Main.java
+++ src/ru/ayupov/task01/Main.java
@@ -3,14 +3,17 @@ package ru.ayupov.task01;
 import java.util.Scanner;
 
 /**
- * Домашнее задание ;4
+ * Домашнее задание №4
+ * Програма №1
  *
- * @author Фирзар Аюпов *
- * Написать программу которая сравнивает два числа и выводит минимальный.
+ * @author Фирзар Аюпов
+ * <p>
+ * Написать программу для поиска минимального из двух чисел.
  */
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Программа поиска минимального числа из двух чисел");
         System.out.println("Введите первое число:");
         Scanner scn = new Scanner(System.in);
         int numOne = scn.nextInt();
